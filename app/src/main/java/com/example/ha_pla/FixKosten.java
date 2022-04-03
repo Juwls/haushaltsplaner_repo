3bb7a5c801bc2d91dc7e7dc1b6b301ab50de62c9
Rikerus <45260136+Rikerus@users.noreply.github.com>
2022-04-03 16:34:43 +0200

    dateien angelegt

---
+++ app/src/main/java/com/example/ha_pla/FixKosten.java
@@ -0,0 +1,15 @@
+package com.example.ha_pla;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class FixKosten extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.finanzfix_layout);
+    }
+}
