ff21ae11aee757de71e689f388f7f716e63c87ab
Juwls <47170056+Juwls@users.noreply.github.com>
2022-04-07 18:04:30 +0200

    refactored einkaufen_layout.xml

--- app/src/main/java/com/example/ha_pla/Einkaufen.java
+++ app/src/main/java/com/example/ha_pla/Einkaufen.java
@@ -2,10 +2,15 @@ package com.example.ha_pla;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class Einkaufen extends Activity {
+
+    private TextView einkaufenTitel;
+    private EditText
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
